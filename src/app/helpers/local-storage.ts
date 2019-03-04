ba474cc7e5a37d0bd651e7a428eff2d793a0945a
Elvir Tabakovic <elvir.tabakovic.92@gmail.com>
2019-03-04 13:20:47 +0100

    rename helper tweak

--- src/app/helpers/local-storage.ts
+++ src/app/helpers/local-storage.ts
@@ -1,4 +1,3 @@
-import { v4 as uuid } from 'uuid';
 import { IUrl } from '../models';
 import { CONSTANTS as _ } from '../config/constants';
 
@@ -15,7 +14,7 @@ export const isEmpty = (object: any): boolean => object && Object.keys(object).l
 
 /** This function gets all urls from local storage. */
 export const getUrls = (): IUrl[] => {
-    let urls;
+    let urls: IUrl[];
     const result = localStorage.getItem(_.URLS);
     if (!isEmpty(result)) {
         try {
@@ -32,10 +31,6 @@ export const getUrls = (): IUrl[] => {
 export const addUrl = (url: IUrl): IUrl => {
     if (!isEmpty(url)) {
         const list = getUrls();
-        // Set default values
-        url.id = uuid(); // or get last id from list and increase it
-        url.insertDate = new Date();
-        url.updateDate = new Date();
         list.push(url);
         updateUrlStorage(list);
         return url;
--- src/app/services/url.service.ts
+++ src/app/services/url.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { v4 as uuid } from 'uuid';
 
 import { IUrl } from '../models';
-import { addUrl, getUrls } from '../helpers/utility';
+import { addUrl, getUrls } from '../helpers/local-storage';
 
 @Injectable()
 export class UrlService {
     constructor(private http: HttpClient) { }
 
     addUrl(url: IUrl): IUrl {
+        // Set default values
+        url.id = uuid(); // or get last id from list and increase it
+        url.insertDate = new Date();
+        url.updateDate = new Date();
         return addUrl(url);
     }
 
